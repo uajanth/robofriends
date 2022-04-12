6d6b324dbc00c2eeb9de92c5ab7bd3ebbc5d72cb
Ajanth Uthayan <uthayakumaran.ajanth@gmail.com>
2022-04-12 11:20:28 -0400

    fetch for users instead of hard coding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import "./App.css"
+import "./App.css";
 import "tachyons";
-import { robots } from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { robots, searchField: "" };
+    this.state = { robots: [], searchField: "" };
   }
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
   };
 
-  render() {
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
 
+  render() {
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name
         .toLowerCase()
