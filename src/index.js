0cae88acc215d4ebafd2c867a571327281601b04
Ajanth Uthayan <uthayakumaran.ajanth@gmail.com>
2022-04-12 12:33:17 -0400

    make changes to index.js to update React to v18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./containers/App";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
